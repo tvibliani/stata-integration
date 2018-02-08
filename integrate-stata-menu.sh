b82cfde0d081c2d29d0a242ae071b22f62e6a138
Temur <tvibliani@users.noreply.github.com>
2018-02-08 05:23:11 +0400

    Install Stata15 icons

--- integrate-stata-menu.sh
+++ integrate-stata-menu.sh
@@ -8,9 +8,18 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
-
+##
+# Add Stata mime types
+##
 cp -uf stata-mimetypes.xml  /usr/share/mime/packages/
 
 update-mime-database /usr/share/mime
+
+##
+# Add icons for stata files and menus
+##
+cp -uf svg/*.svg /usr/share/icons/gnome/scalable/mimetypes/
+
+gtk-update-icon-cache /usr/share/icons/gnome -f
 
 
